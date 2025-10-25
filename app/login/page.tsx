7dc528dc7f94b4a172ce162de063af0cbb08a0f0
Fernando Alvarez <fernando_0694@hotmail.com>
2025-10-25 13:46:46 -0400

    feat(auth): prepare deployment and Spotify OAuth integration

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
+"use client";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Login() {
+  const handleLogin = () => {
+    window.location.href =
+      "https://accounts.spotify.com/authorize?client_id=dad8def7087741a486c1873745f680fd&response_type=code&redirect_uri=https://luis-alvarez-full-stack.vercel.app/login&scope=user-read-email%20user-library-read";
+  };
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -18,16 +23,19 @@ export default function Home() {
             </div>
           </div>
           <div className={styles.textContent}>
-            <h2 className={styles.heading}>
-              <span className={styles.headingWhite}>Disfruta de la</span>
-              <br />
-              <span className={styles.headingGreen}>mejor música</span>
-            </h2>
-            <p className={styles.description}>
-              Accede a tu cuenta para guardar tus álbumes favoritos.
-            </p>
-            <button className={styles.loginButton}>
+            <div>
+              <h2 className={styles.heading}>
+                <span className={styles.headingWhite}>Disfruta de la</span>
+                <br />
+                <span className={styles.headingGreen}>mejor música</span>
+              </h2>
+              <p className={styles.description}>
+                Accede a tu cuenta para guardar tus álbumes favoritos.
+              </p>
+            </div>
+            <button className={styles.loginButton} onClick={handleLogin}>
               <span className={styles.loginText}>Log in con Spotify</span>
+              <img src="/assets/whiteArrow.svg" className={styles.arrowSmall} />
             </button>
           </div>
         </div>
